45370e4f7260ee1c25dccd026f4f929e058a6169
afmireski <afmireski@outlook.com>
2023-11-26 21:30:27 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # Instalando dependências
 - [Instalar o pandas](https://pandas.pydata.org/docs/getting_started/install.html)
 - [Instalar o matplotlib](https://matplotlib.org/stable/users/installing/index.html)
+- [Instalar o seaborn](https://matplotlib.org/stable/users/installing/index.html)
 ```bash
 # Arch
 sudo pacman -S python-pandas
 
 sudo pacman -S python-matplotlib
 
+sudo pacman -S python-seaborn
+
 # PyPi
 pip install pandas
 
 pip install matplotlib
+
+pip install seaborn
 ```
 
 # Obtendo os dados
 Os gráficos são montados a partir do processamento de dois arquivos CSVs que contém o número de transações diárias das redes Ethereum e Bitcoin.
 Esses arquivos foram gerados a partir da consulta em datasets públicos com dados das duas redes disponíveis no Google BigQuery.
+No caso do Ethereum existem dois datasets, mas ambos geram o mesmo resultado, ou seja, são equivalentes.
 O intervalo de consulta dos dados é o que está na condição das queries abaixo.
 
 ## Queries
@@ -44,3 +50,11 @@ ORDER BY timestamp1
 ```
 
 # Rodando o experimento
+1. Escolha quais CSVs deseja utilizar. Comente e descomente os elementos em `datasets`.
+2. Execute `experimento.py` chamando `python ./experimento.py` ou use seu editor.
+3. O resultado esperado é um gráfico com o número de transações diárias de cada dataset.
+
+# Objetivos e conclusões
+O objetivo desse experimento é replicar o gráfico da figura 3, presente no artigo de ABDULLAH A. ZARIR et.al. Os autores em teoria utilizaram os mesmos datasets disponíveis no Google BigQuery para desenvolver sua tese.
+
+Porém o que observei foi uma grande discrepância no gráfico, para o mesmo período. Como os dados originais aparentemente não foram disponibilizados para os pesquisadores fica difícil saber se a discrepância se dá por conta de mudanças no dataset ou por falha minha.
--- experimento.py
+++ experimento.py
@@ -1,20 +1,55 @@
 import pandas as pd
-
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+
 from functools import reduce
 
-datasets = ['ethereum', 'bitcoin']
+datasets = [
+    'ethereum',
+    'ethereum-mainnet-us',
+    'bitcoin'
+    ]
 dataframes = []
 
 # Read BigQuery CSV data
 for dataset in datasets:
     df = pd.read_csv(f'data/{dataset}-transactions.csv', index_col=0, parse_dates=True)
-    dataframes.append(df.rename(columns={'txn_count':f'{dataset}_txn'})) # Rename default column name to eth_txn
+    df['dataset'] = dataset; # Adiciona uma nova coluna com o nome do dataset em cada linha
+    dataframes.append(df) 
 
 
-txn_df = reduce(lambda left,right: pd.merge(left, right, on='timestamp1'),dataframes)  # Merge all datasets
+txn_df = pd.concat(dataframes)  # Concatena os datasets
+txn_df.to_csv('./output/txt_df.csv')
 
-txn_df.plot(kind='line')
-plt.xlabel('Time')
-plt.ylabel('Txs Count')
+# cria axis para controlar melhor elementos do grafico
+fig, ax = plt.subplots()
+
+# remove as bordas para melhor visualizacao
+sns.despine(right=True, top=True, ax=ax)
+sns.set_style('ticks')
+
+
+sns.lineplot(x='timestamp1', y='txn_count', hue='dataset', data=txn_df, ax=ax, legend='brief')
+
+
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) # Define o intervalo entre cada ponto do eixo x em 1 mês
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y')) # Configura o label do eixo x
+plt.xticks(rotation=90, ha='right')
+
+# Define o título e os labels dos eixos
+ax.set_title('Número de transações diárias por criptomoeda')
+ax.set_xlabel('Time')
+
+# Define um limite pros dados, removendo espaços em branco.
+ax.set_xlim(left=txn_df.index[0], right=txn_df.index[len(txn_df.index)-1])
+
+
+ax.set_ylabel('Txn Count')
+ax.set
+ax.legend(ncol=1, frameon=False, fontsize=12)
+
+
 plt.show()
+fig.tight_layout()
+fig.savefig('./output/experimentos.pdf') # Salva o gráfico como PDF
